2fc84db0d91651c0d0dac9d846eec10fc9e2c49d
spencer <spencer.bigum@gmail.com>
2016-06-19 01:47:14 -0400

    begin todo-app

---
+++ todo-app/app/app.jsx
@@ -0,0 +1,25 @@
+import react from 'react';
+const React = react;
+var ReactDOM = require('react-dom');
+import {Route, Router, IndexRoute, hashHistory} from 'react-router';
+
+// Import Componentss
+
+
+// Load foundation manually
+// require('style!css!foundation-sites/dist/foundation.min.css');
+$(document).foundation();
+
+// Load Custom App CSS
+require('style!css!sass!applicationStyles');
+
+ReactDOM.render(
+    <p>Boilerplate 3 project</p>,
+  // <Router history={hashHistory}>
+  //     <Route path="/" component={Main}>
+  //         <Route path="countdown" component={Countdown} />
+  //         <IndexRoute component={Timer}/>
+  //     </Route>
+  // </Router>,
+  document.getElementById('app')
+);
---
+++ todo-app/webpack.config.js
@@ -0,0 +1,57 @@
+var LiveReloadPlugin = require('webpack-livereload-plugin');
+var options = {
+  port: 35729
+};
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+  entry: [
+    'script!jquery/dist/jquery.min.js',
+    'script!foundation-sites/dist/foundation.min.js',
+    './app/app.jsx'
+  ],
+  output: {
+    path: __dirname,
+    filename: './public/bundle.js'
+  },
+  resolve: {
+    //tell where everything is
+    root: __dirname,
+    alias: {
+      applicationStyles: 'app/styles/app.scss',
+    },
+    extensions: [
+      '', '.js', '.jsx', '.html'
+    ]
+  },
+  externals: {
+    jquery: 'jQuery'
+  },
+  plugins: [
+    new LiveReloadPlugin(options),
+    new webpack.ProvidePlugin({
+      '$': 'jquery',
+      'jQuery': 'jquery'
+    })
+  ],
+  module: {
+    //loaders
+    loaders: [
+      {
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-0']
+        },
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/
+      }
+    ]
+  },
+  sassLoader:{
+    includePaths: [
+        path.resolve( __dirname, './node_modules/foundation-sites/scss')
+    ]
+  },
+  devtool: 'cheap-module-eval-source-map'
+};
